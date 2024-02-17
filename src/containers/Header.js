e5216557effbbe57a5ac1b40774f00743a63834e
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-02-17 12:46:18 +0530

    Routings added

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -3,8 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div className="ui fixed menu">
-      <div className="ui container center"></div>
-      <h2>Fake Shop</h2>
+      <div className="ui container center">
+        <h2>Fake Shop</h2>
+      </div>
     </div>
   );
 };
